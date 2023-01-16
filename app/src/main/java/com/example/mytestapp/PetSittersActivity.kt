df871fdd918a5fd3d31e9b514ebffbff44f0d776
KevinAxell <18170314@itculiacan.edu.mx>
2023-01-15 20:33:51 -0700

    AppFinal - Simulacion de cita y olvidaste contra

--- app/src/main/java/com/example/mytestapp/PetSittersActivity.kt
+++ app/src/main/java/com/example/mytestapp/PetSittersActivity.kt
@@ -2,6 +2,8 @@ package com.example.mytestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_pet_sitters.*
 
 class PetSittersActivity : AppCompatActivity() {
@@ -14,6 +16,10 @@ class PetSittersActivity : AppCompatActivity() {
         //Mostrar el toolbar
         MyToolBar().show(this,"${petSitter.name}",true,true)
 
+
+        val scheduleButton : Button = findViewById(R.id.btn_schedule)
+
+        scheduleButton.setOnClickListener { Toast.makeText(this,"Success Schedule", Toast.LENGTH_SHORT).show() }
 
         pet_sitter_name.text = petSitter.name
         pet_sitter_age.text = "${petSitter.age}" + " years"
